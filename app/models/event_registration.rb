772af09aabbc639031a17b6b679c9356a940bb8b
Sheila Leveille <sbleveille@gmail.com>
2019-06-03 17:51:03 +1000

    quick model relations

--- app/models/event_registration.rb
+++ app/models/event_registration.rb
@@ -1,4 +1,12 @@
 class EventRegistration < ApplicationRecord
   belongs_to :event
   belongs_to :user
+
+  enum status: %i[open full]
+
+  private
+
+  def full
+    self.update(status: :full)
+  end
 end
