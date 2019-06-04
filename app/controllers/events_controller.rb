ff2b51e6d46ff167783ba3fccab690b75402fa3f
Sheila Leveille <sbleveille@gmail.com>
2019-06-04 10:39:32 +1000

    show routes and uncomment map

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -4,11 +4,11 @@ class EventsController < ApplicationController
 
   def index
     @events = Event.all
-    events_markers
+    # event_markers
   end
 
   def show
-    events_markers
+    # event_markers
   end
 
   def create
