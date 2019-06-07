7c3da59875cfc7f52ad27cb98f993880a7947fc6
Sheila Leveille <sbleveille@gmail.com>
2019-06-07 12:25:12 +1000

    udpates to the cards and photos for activities

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_06_014454) do
+ActiveRecord::Schema.define(version: 2019_06_06_235903) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2019_06_06_014454) do
     t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "photo"
   end
 
   create_table "event_favorites", force: :cascade do |t|
