febf8c910d7856ba5a1f7e4ef87a3a783a683b00
Eu-Loong Lim <euloong@hotmail.com>
2019-06-10 17:20:22 +1000

    search functionality added to locations index

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -2,8 +2,16 @@ class LocationsController < ApplicationController
   before_action :set_location, only: :show
 
   def index
-    @locations = Location.all
-    @locations_sorted = @locations.sort_by_events
+    @all_locations = Location.all
+    @locations_sorted = @all_locations.sort_by_events
+    # required for search
+    unless params[:query].nil?
+      @query = true
+      @locations = Location.where('city ILIKE ?', "%#{params[:query]}%")
+    else
+      @query = false
+      @locations = Location.all
+    end
   end
 
   def show
