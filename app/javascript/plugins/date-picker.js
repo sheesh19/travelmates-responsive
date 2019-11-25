b6c3bb3d79a6da55ec049555d8403028c4a8bcfb
Sheila Leveille <sbleveille@gmail.com>
2019-11-25 17:19:26 +1100

    updated the flatpickr for profile

--- app/javascript/plugins/date-picker.js
+++ app/javascript/plugins/date-picker.js
@@ -1,14 +1,14 @@
 import flatpickr from "flatpickr";
 
 // Source: https://flatpickr.js.org/
-flatpickr("input[type='date']", {
-  altInput: true,
-  altFormat: "D, F j, Y",
-  dateFormat: "Y-m-d"
-})
-flatpickr("input[type='datetime-local']", {
-  enableTime: true,
-  altInput: true,
-  altFormat: "D, F j, Y h:iK",
-  dateFormat: "Y-m-d\\TH:i"
-})
+
+flatpickr("#user_date_of_birth", {
+  // altInput: true
+});
+
+// flatpickr(".date-time-picker", {
+//   // enableTime: true,
+//   // altFormat: "D, F j, Y h:iK",
+//   // dateFormat: "Y-m-d\\TH:i",
+//   // 
+// })
